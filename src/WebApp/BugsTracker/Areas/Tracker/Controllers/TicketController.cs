a4525a28f1a0be0e14c616fa497f9f4b33824575
Quentin Couissinier <reliabledevelopment@outlook.com>
2022-01-31 10:04:49 +0100

    Views of dasboard and tickets updated. Particular attention on ticket details.

---
+++ src/WebApp/BugsTracker/Areas/Tracker/Controllers/TicketController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BugTracker.Areas.Tracker.Controllers
+{
+    [Area("Tracker")]
+
+    public class TicketController : Controller
+    {
+        public IActionResult Details(int Id)
+        {
+            return View();
+        }
+    }
+}
