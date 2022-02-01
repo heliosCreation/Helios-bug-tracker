eb1869ce80c85e83a17b72a245b466ed52662f60
Quentin Couissinier <reliabledevelopment@outlook.com>
2022-02-01 14:03:56 +0100

    Created AuditableEntity base class in domain layer.

---
+++ src/BugTracker.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BugTracker.Domain.Common
+{
+    public class AuditableEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+        public Guid CreatedBy { get; set; }
+        public Guid? LastModifiedBy { get; set; }
+
+    }
+}
