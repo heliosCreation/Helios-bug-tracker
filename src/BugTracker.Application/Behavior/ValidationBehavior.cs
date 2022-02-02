cd7ffd467ac1c1ff5500f3bb623843b298dc069a
Quentin Couissinier <reliabledevelopment@outlook.com>
2022-02-02 13:30:58 +0100

    Added Validation Behavior.

---
+++ src/BugTracker.Application/Behavior/ValidationBehavior.cs
@@ -0,0 +1,48 @@
+﻿using BugTracker.Application.Responses;
+using FluentValidation;
+using MediatR;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BugTracker.Application.Behavior
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+            where TRequest : IRequest<TResponse>
+            where TResponse : BaseResponse, new()
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            if (_validators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+
+                var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+                var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+
+
+                if (failures.Count != 0)
+                {
+
+                    return new TResponse
+                    {
+                        Succeeded = false,
+                        StatusCode = (int)HttpStatusCode.BadRequest,
+                        ErrorMessages = failures.Select(f => f.ErrorMessage).ToList()
+                    };
+                }
+            }
+            return await next();
+
+        }
+    }
+}
