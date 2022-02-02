edae3b143f12974079423a11cd5e8a09b30f0f9d
Quentin Couissinier <reliabledevelopment@outlook.com>
2022-02-02 12:03:17 +0100

    Added persistence service registration and registered the service.

---
+++ src/BugTracker.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using BugTracker.Application.Contracts.Data;
+using BugTracker.Persistence.Services.Data;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BugTracker.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceService(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(BaseRepository<>));
+
+            services.AddScoped<IProjectRepository, ProjectRepository>();
+            services.AddScoped<ITicketRepository, TicketRepository>();
+            services.AddScoped<ICommentRepository, CommentRepository>();
+
+            return services;
+        }
+    }
+}
