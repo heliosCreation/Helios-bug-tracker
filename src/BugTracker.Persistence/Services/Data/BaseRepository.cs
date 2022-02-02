0bffd7c38de59058778e4d82cb7599096a7056e2
Quentin Couissinier <reliabledevelopment@outlook.com>
2022-02-02 11:31:31 +0100

    Created base interface and repository for the data interractions.

---
+++ src/BugTracker.Persistence/Services/Data/BaseRepository.cs
@@ -0,0 +1,55 @@
+﻿using BugTracker.Application.Contracts.Data;
+using BugTracker.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Persistence.Services.Data
+{
+    public class BaseRepository<T> : IAsyncRepository<T> where T : AuditableEntity
+    {
+        protected readonly ApplicationDbContext _dbContext;
+
+        public BaseRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public virtual async Task<T> AddAsync(T entity)
+        {
+            await _dbContext.Set<T>().AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public virtual async Task DeleteAsync(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public virtual async Task<T> GetByIdAsync(Guid id)
+        {
+            return await _dbContext.Set<T>().AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public virtual async Task<T> GetOnly()
+        {
+            return await _dbContext.Set<T>().AsNoTracking().FirstOrDefaultAsync();
+        }
+
+        public virtual async Task<IEnumerable<T>> ListAllAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+
+        public virtual async Task UpdateAsync(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
