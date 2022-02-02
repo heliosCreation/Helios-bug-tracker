60f9f6fe9d4eeef822d1ea5d0794b0249ad3aea1
Quentin Couissinier <reliabledevelopment@outlook.com>
2022-02-02 15:16:32 +0100

    Delete project command and handler set up.

---
+++ src/BugTracker.Application/Features/Projects/Commands/Delete/DeleteProjectCommand.cs
@@ -0,0 +1,11 @@
+﻿using BugTracker.Application.Responses;
+using MediatR;
+using System;
+
+namespace BugTracker.Application.Features.Projects.Commands.Delete
+{
+    public class DeleteProjectCommand : IRequest<ApiResponse<object>>
+    {
+        public Guid Id { get; set; }
+    }
+}
