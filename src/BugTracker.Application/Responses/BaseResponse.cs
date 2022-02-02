f4678379a2aa58e803dc5993fa7ba99da6599a44
Quentin Couissinier <reliabledevelopment@outlook.com>
2022-02-02 13:28:12 +0100

    Added BaseResponse and ApiResponse class to wrap request results.

---
+++ src/BugTracker.Application/Responses/BaseResponse.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace BugTracker.Application.Responses
+{
+    public class BaseResponse
+    {
+        public BaseResponse()
+        {
+            Succeeded = true;
+        }
+
+        public BaseResponse(string message = null)
+        {
+            Succeeded = true;
+            Message = message;
+        }
+
+        public BaseResponse(string message, bool success)
+        {
+            Succeeded = success;
+            Message = message;
+        }
+
+        public bool Succeeded { get; set; }
+
+        public string Message { get; set; }
+
+        public int StatusCode { get; set; } = (int)HttpStatusCode.OK;
+
+        public List<string> ErrorMessages { get; set; } = new List<string>();
+    }
+}
