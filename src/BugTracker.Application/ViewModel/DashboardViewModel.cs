aac8b61551394388344fe542e4518e901e7884c9
Quentin Couissinier <reliabledevelopment@outlook.com>
2022-02-03 11:34:57 +0100

    Added dashboard view model and baseController to hold mediatr dependency.

---
+++ src/BugTracker.Application/ViewModel/DashboardViewModel.cs
@@ -0,0 +1,11 @@
+﻿using BugTracker.Application.Features.Projects;
+using System.Collections.Generic;
+
+namespace BugTracker.Application.ViewModel
+{
+    public class DashboardViewModel
+    {
+        public ICollection<ProjectVm> Projects { get; set; }
+        //public ICollection<TicketVm> Tickets { get; set; }
+    }
+}
---
+++ src/WebApp/BugsTracker/Areas/Tracker/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BugTracker.Areas.Tracker.Controllers
+{
+    public class BaseController : Controller
+    {
+        private readonly IMediator _mediator;
+        protected IMediator Mediator => _mediator ?? HttpContext.RequestServices.GetService<IMediator>();
+    }
+}
