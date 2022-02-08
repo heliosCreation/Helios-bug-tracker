7bef38c523f640697c9c73367eef7e955b1e2069
Quentin Couissinier <reliabledevelopment@outlook.com>
2022-02-08 10:37:49 +0100

    Audit applied to all element linked to the database.

--- src/BugTracker.Persistence/ApplicationDbContext.cs
+++ src/BugTracker.Persistence/ApplicationDbContext.cs
@@ -1,17 +1,29 @@
-﻿using BugTracker.Domain.Entities;
+﻿using BugTracker.Application.Contracts.Identity;
+using BugTracker.Domain.Common;
+using BugTracker.Domain.Entities;
 using BugTracker.Domain.Identity;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Reflection;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace BugTracker.Persistence
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IHttpContextAccessor httpContextAccessor)
             : base(options)
         {
+            _httpContextAccessor = httpContextAccessor;
+            
         }
+
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -24,5 +36,25 @@ namespace BugTracker.Persistence
         public DbSet<ProjectTeamMember> ProjectTeamMembers { get; set; }
         public DbSet<Ticket> Tickets { get; set; }
         public DbSet<Comment> Comments { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries<AuditableEntity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        entry.Entity.CreatedBy = Guid.Parse(_httpContextAccessor.HttpContext.User?.FindFirstValue(ClaimTypes.NameIdentifier));
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        entry.Entity.LastModifiedBy = Guid.Parse(_httpContextAccessor.HttpContext.User?.FindFirstValue(ClaimTypes.NameIdentifier));
+                        break;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
     }
 }
