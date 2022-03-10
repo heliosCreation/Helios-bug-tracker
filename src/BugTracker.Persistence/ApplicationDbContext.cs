3f60619e2f5de07e6cbfe2bcf0aa85c3a6020358
Quentin Couissinier <reliabledevelopment@outlook.com>
2022-03-10 09:59:59 +0100

    Removed log methods from dbContext.

--- src/BugTracker.Persistence/ApplicationDbContext.cs
+++ src/BugTracker.Persistence/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using BugTracker.Application.Model.Auditing;
 using BugTracker.Domain.Common;
 using BugTracker.Domain.Entities;
 using BugTracker.Domain.Identity;
+using BugTracker.Persistence.LogHelpers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -53,7 +54,8 @@ namespace BugTracker.Persistence
 
         public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
         {
-            await OnBeforeSaveChangesAsync(_httpContextAccessor.HttpContext.User?.FindFirstValue(ClaimTypes.NameIdentifier));
+            var logHelper = new LogHelper(this);
+            await logHelper.OnBeforeSaveChangesAsync(_httpContextAccessor.HttpContext.User?.FindFirstValue(ClaimTypes.NameIdentifier));
 
             foreach (var entry in ChangeTracker.Entries<AuditableEntity>())
             {
@@ -70,98 +72,6 @@ namespace BugTracker.Persistence
                 }
             }
             return await base.SaveChangesAsync(cancellationToken);
-        }
-
-        private async Task OnBeforeSaveChangesAsync(string userId)
-        {
-            ChangeTracker.DetectChanges();
-            var auditEntries = new List<AuditEntry>();
-
-            await MapEntries(auditEntries, userId);
-
-            foreach (var auditEntry in auditEntries)
-            {
-                var auditLog = auditEntry.ToAudit();
-                await AuditLogs.AddAsync(auditLog);
-            }
-        }
-
-
-        private async Task MapEntries(List<AuditEntry> auditEntries, string userId)
-        {
-            foreach (var entry in ChangeTracker.Entries())
-            {
-                if (entry.Entity is Audit || entry.State == EntityState.Detached || entry.State == EntityState.Unchanged)
-                    continue;
-
-                var auditEntry = new AuditEntry(entry);
-                auditEntry.TableName = entry.Entity.GetType().Name;
-                auditEntry.UserId = userId;
-                auditEntries.Add(auditEntry);
-                await MapPropertiesToEntryValue(entry, auditEntry);
-            }
-
-        }
-    
-        private async Task MapPropertiesToEntryValue(EntityEntry entry, AuditEntry auditEntry)
-        {
-            foreach (var property in entry.Properties)
-            {
-                var current = property.CurrentValue;
-
-                MapPKToAuditEntryValue(property, auditEntry, current);
-                await MapUserIdToLogValuesIfNecessary(property, current);
-                AssignValuesBasedOnEntryState(auditEntry, current, property, entry);
-            }
-
-        }
-        
-        private void MapPKToAuditEntryValue(PropertyEntry property, AuditEntry auditEntry, object current)
-        {
-            if (property.Metadata.IsPrimaryKey())
-            {
-                auditEntry.KeyValues[property.Metadata.Name] = current;
-            }
-        }
-        private async Task MapUserIdToLogValuesIfNecessary(PropertyEntry property, object current)
-        {
-            if (property.Metadata.Name == "UserId")
-            {
-                var rolesId = await this.UserRoles.Where(ur => ur.UserId == (string)property.CurrentValue).Select(ur => ur.RoleId).ToListAsync();
-                var roles = await this.Roles.Where(r => rolesId.Any(roleId => r.Id == roleId)).Select(r => r.Name).ToListAsync();
-
-                current = JsonSerializer.Serialize(new
-                {
-                    Id = property.CurrentValue,
-                    Name = await this.Users.Where(u => u.Id == (string)property.CurrentValue).Select(u => u.UserName).FirstOrDefaultAsync(),
-                    Roles = roles
-                });
-            }
-        }
-    
-        private void AssignValuesBasedOnEntryState(AuditEntry auditEntry, object current, PropertyEntry property, EntityEntry entry)
-        {
-            switch (entry.State)
-            {
-                case EntityState.Added:
-                    auditEntry.AuditType = Application.Enums.AuditType.Create;
-                    auditEntry.NewValues[property.Metadata.Name] = current;
-                    break;
-                case EntityState.Deleted:
-                    auditEntry.AuditType = Application.Enums.AuditType.Delete;
-                    auditEntry.OldValues[property.Metadata.Name] = current;
-                    break;
-                case EntityState.Modified:
-                    if (property.IsModified)
-                    {
-                        auditEntry.ChangedColumns.Add(property.Metadata.Name);
-                        auditEntry.AuditType = Application.Enums.AuditType.Update;
-                        auditEntry.OldValues[property.Metadata.Name] = property.OriginalValue;
-                        auditEntry.NewValues[property.Metadata.Name] = current;
-                    }
-                    break;
-            }
-
-        }
+        }  
     }
 }
